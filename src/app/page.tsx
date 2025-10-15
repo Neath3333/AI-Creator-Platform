26b6aef47cb228cead773fee299556708183b7d3
Neath Soda <daneath@saerosoft.com>
2025-10-16 01:38:48 +0700

    feat: create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+"use client"
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button"; 
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [mousePosition, setMousePosition] = useState({x:0, y:0});
+
+    useEffect (() => {
+      const handleMouseMove = (e) => {
+        setMousePosition({x:e.clientX, y:e.clientY }) 
+      };
+    window.addEventListener("mousemove", handleMouseMove);  
+
+     return ()=>{
+      window.removeEventListener("mouseMove", handleMouseMove);
+     };
+  
+    },[]);
+
   return (
-    <Button variant={"gradient"} size={"xl"}>Start Creating for Free</Button>
+    <div className="min-h-screen bg-black text-white overflow-hidden relative">
+      <div className="fixed inset-0 bg-gradient-to-br from-blue-900/20 via-purple-900/20 t0-green-900/20 animate-pulse"/>
+    </div>
   );
 }
