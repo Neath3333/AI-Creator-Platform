718f1f93bc006d905f99058b0a1ec4b88ddd30a4
Neath Soda <daneath@saerosoft.com>
2025-10-16 01:42:28 +0700

    fix: mousemove event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
   const [mousePosition, setMousePosition] = useState({x:0, y:0});
 
     useEffect (() => {
-      const handleMouseMove = (e) => {
+      const handleMouseMove = (e:MouseEvent) => {
         setMousePosition({x:e.clientX, y:e.clientY }) 
       };
     window.addEventListener("mousemove", handleMouseMove);  
 
      return ()=>{
-      window.removeEventListener("mouseMove", handleMouseMove);
+      window.removeEventListener("mousemove", handleMouseMove);
      };
   
     },[]);
