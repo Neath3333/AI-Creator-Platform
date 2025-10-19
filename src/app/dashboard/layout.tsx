7914dd45391300976a4ea607e2559a81ffe2aebd
Neath Soda <daneath@saerosoft.com>
2025-10-19 17:34:17 +0700

    feat: create dashboard layout and page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import { Divide } from "lucide-react";
+import React from "react";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div>{children}</div>
+    )
+}
+
+export default DashboardLayout;
